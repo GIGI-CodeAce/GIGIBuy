6e378249acb31c9042eb10a872833700914f7df0
Dobre Robert <dobrerobert441@gmail.com>
2025-02-04 17:06:17 +0200

    fixed elements: navBar,offer,etc..

---
+++ src/Fixed/specialOffer.tsx
@@ -0,0 +1,10 @@
+
+function SpecialOffer(){
+    return(
+        <main className="bg-orange-400 text-center font-bold text-white"> 
+            OMG chat HUGE discount
+        </main>
+    )
+}
+
+export default SpecialOffer
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import NavigationBar from './Fixed/navBar.tsx'
+import SpecialOffer from './Fixed/specialOffer.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
+   <>
+   <NavigationBar/>
+   <SpecialOffer/>
     <App />
+   </>
 )
