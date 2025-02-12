a69a877f5a28886e6fcdf72576aeb379360d77ec
Dobre Robert <dobrerobert441@gmail.com>
2025-02-12 15:20:55 +0200

    gh pages final

--- src/Fixed/navBar.tsx
+++ src/Fixed/navBar.tsx
@@ -7,7 +7,7 @@ function NavigationBar({ onSearch }: { onSearch: (query: string) => void }) {
 
   return (
     <nav className="w-full pt-0 h-20 bg-[#455d7a] flex justify-evenly items-center text-[15px] sm:text-[20px] text-white">
-      <a href="./">
+      <a href="../">
         <h1 className="text-2xl active:text-[#e5c9c3] font-bold ml-2 sm:text-[25px] flex items-center gap-1 mr-2">
           <div className="w-10 h-10 bg-cover bg-center bg-[url('https://raw.githubusercontent.com/GIGIsOtherStuff/GIGIbuy/main/diamond.png')]"></div>
           GIGIbuy
